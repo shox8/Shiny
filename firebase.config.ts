737576779adeca1588846c9ec7d7d569a4feabae
Shox2008 <codershoxruh@gmail.com>
2024-04-25 17:56:44 +0500

    first commit

---
+++ firebase.config.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBcEkcuvLzO6cHok5pJyYyLA6aPUhtQSAs",
+  authDomain: "shiny-17858.firebaseapp.com",
+  projectId: "shiny-17858",
+  storageBucket: "shiny-17858.appspot.com",
+  messagingSenderId: "724204705511",
+  appId: "1:724204705511:web:506279c20916fc3a60b442",
+  measurementId: "G-N1JZB91X5B",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
