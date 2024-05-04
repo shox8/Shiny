5a646079f1f9b5d00b673d2ec5d63d590146ad8d
Shoxruh <138918930+Shox2008@users.noreply.github.com>
2024-05-04 18:45:59 +0500

    Updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,12 @@
 "use client";
 import { userId } from "./base";
 import { useMemberQuery } from "@/redux/services/auth";
-import { useEffect } from "react";
 import styles from "../styles/layout.module.scss";
 import Profile from "@/components/ProfileBlog";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
-  useEffect(() => {
-    const {} = useMemberQuery(userId || "");
-  }, []);
+  const {} = useMemberQuery(userId);
 
   return (
     <div className={styles.layout}>
